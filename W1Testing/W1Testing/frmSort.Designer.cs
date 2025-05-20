f43e8460add1feab92a870fd01f6fed098795954
Boone <68450612+Expressbadger12@users.noreply.github.com>
2025-05-20 11:20:01 -0700

    Add files via upload

---
+++ W1Testing/W1Testing/frmSort.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace W1Testing
+{
+    partial class frmSort
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblBubbleSort = new Label();
+            lblSelectionSort = new Label();
+            btnSort = new Button();
+            SuspendLayout();
+            // 
+            // lblBubbleSort
+            // 
+            lblBubbleSort.AutoSize = true;
+            lblBubbleSort.Location = new Point(325, 150);
+            lblBubbleSort.Name = "lblBubbleSort";
+            lblBubbleSort.Size = new Size(174, 25);
+            lblBubbleSort.TabIndex = 0;
+            lblBubbleSort.Text = "Run Sort to see time";
+            // 
+            // lblSelectionSort
+            // 
+            lblSelectionSort.AutoSize = true;
+            lblSelectionSort.Location = new Point(325, 187);
+            lblSelectionSort.Name = "lblSelectionSort";
+            lblSelectionSort.Size = new Size(174, 25);
+            lblSelectionSort.TabIndex = 1;
+            lblSelectionSort.Text = "Run Sort to see time";
+            // 
+            // btnSort
+            // 
+            btnSort.Location = new Point(285, 261);
+            btnSort.Name = "btnSort";
+            btnSort.Size = new Size(232, 34);
+            btnSort.TabIndex = 2;
+            btnSort.Text = "Run Sorting";
+            btnSort.UseVisualStyleBackColor = true;
+            btnSort.Click += this.btnSort_Click;
+            // 
+            // frmSort
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSort);
+            Controls.Add(lblSelectionSort);
+            Controls.Add(lblBubbleSort);
+            Name = "frmSort";
+            Text = "Sorting Fun";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblBubbleSort;
+        private Label lblSelectionSort;
+        private Button btnSort;
+    }
+}
---
+++ W1Testing/W1Testing/frmSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W1Testing
+{
+    public partial class frmSort : Form
+    {
+        public frmSort()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            //generate a random array as specified by these inputs
+            int[] iarrOriginalArray = GenerateRandomArray(1000, 1, 10000);
+
+            //create 2 coppies of the random array by our sorting algortuthisn
+            int[] irrBubble = (int[])iarrOriginalArray.Clone();
+            int[] irrSelection = (int[])iarrOriginalArray.Clone();
+
+        }
+
+
+        // create a function to generate an array with a random number of elements between specified min and max
+        private int[] GenerateRandomArray(int iSize, int iMin, int iMax)
+        {
+            //set up a random object so we can get random numbers
+            Random random = new Random();
+
+            //set up an array with the size specified when this function is called
+            int[] iarrRandomNumbers = new int[iSize];
+
+            //fill our array with random numbers
+            for (int i = 0; i < iSize; i++)
+            {
+                // generate a random number between the specified min and max and assign that random number to the current elements in the array
+                iarrRandomNumbers[i] = random.Next(iMin, iMax);
+            }
+
+            //retunr our array with teh random numbers
+            return iarrRandomNumbers;
+        }
+    }
+}
